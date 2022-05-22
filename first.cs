017fbbb83f6f2c0ff6e1186efd76a4422a9cc3a5
진기 <jingit38@gmail.com>
2022-05-23 01:48:32 +0900

    mrBeak

---
+++ first.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Baekjoon_1
+{
+    public class first
+    {
+        public static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
+        }
+    }
+}
